fa18c88a579416fd9392a1942fc5ab31512aff81
Ritik Shroff <109219868+Ritikshroff@users.noreply.github.com>
2025-01-08 14:01:01 +0530

    changes

--- src/components/Dashboard/DashboardLayout.jsx
+++ src/components/Dashboard/DashboardLayout.jsx
@@ -3,6 +3,7 @@
 import Sidebar from './Sidebar';
 // import TopNavBar from './TopNavBar';  // A top navbar with the logo, user profile, and other quick links
 import DashboardContent from './DashboardContent'; // This is the main section where content (charts, stats) will be shown
+// import AnalyticsOverview from './Analyticsoverview';
 import AnalyticsOverview from './Analyticsoverview';
 import CampaignDashboard from './CampaignDashboard';
 
@@ -15,6 +16,7 @@ const DashboardLayout = ({ role }) => {
             {/* Main Content */}
             <div className="flex-1 bg-white p-6 overflow-auto">
                 <DashboardContent />
+                {/* <AnalyticsOverview/> */}
                 <AnalyticsOverview/>
                 <CampaignDashboard/>
             </div>
