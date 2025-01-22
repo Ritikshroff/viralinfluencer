2a8edf0a8c7e692478a6d5e9eebc2b14145ba4c8
Ritik Shroff <109219868+Ritikshroff@users.noreply.github.com>
2025-01-22 16:23:28 +0530

    added sitemap

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -1,14 +1,43 @@
-const { SitemapStream, streamToPromise } = require('sitemap');
-const { createWriteStream } = require('fs');
+// const { SitemapStream, streamToPromise } = require('sitemap');
+// const { createWriteStream } = require('fs');
+
+// async function generateSitemap() {
+//   const links = [
+//     { url: '/', changefreq: 'daily', priority: 1.0 },
+//     { url: '/about', changefreq: 'weekly', priority: 0.8 },
+//     { url: '/Services', changefreq: 'weekly', priority: 0.8 },
+//     { url: '/submit-form', changefreq: 'monthly', priority: 0.7 },
+//     { url: '/Influencer', changefreq: 'monthly', priority: 0.7 },
+//     // { url: '/submit-form', changefreq: 'monthly', priority: 0.7 },
+//     // Add more pages as needed
+//   ];
+
+//   const sitemapStream = new SitemapStream({ hostname: 'https://viralfluencer.com' });
+
+//   links.forEach((link) => {
+//     sitemapStream.write(link);
+//   });
+
+//   sitemapStream.end();
+
+//   const sitemap = await streamToPromise(sitemapStream).then((data) => data.toString());
+//   createWriteStream('./public/sitemap.xml').write(sitemap);
+//   console.log('Sitemap successfully generated!');
+// }
+
+// generateSitemap();
+
+
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
 
 async function generateSitemap() {
   const links = [
     { url: '/', changefreq: 'daily', priority: 1.0 },
     { url: '/about', changefreq: 'weekly', priority: 0.8 },
-    { url: '/Services', changefreq: 'weekly', priority: 0.8 },
+    { url: '/services', changefreq: 'weekly', priority: 0.8 },
     { url: '/submit-form', changefreq: 'monthly', priority: 0.7 },
-    { url: '/Influencer', changefreq: 'monthly', priority: 0.7 },
-    // { url: '/submit-form', changefreq: 'monthly', priority: 0.7 },
+    { url: '/influencer', changefreq: 'monthly', priority: 0.7 },
     // Add more pages as needed
   ];
 
